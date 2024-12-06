c06c1b3b89d28323a7739913b04caa538d7b2bf8
manje22 <uvodicmariela@gmail.com>
2024-12-06 10:25:44 +0100

    added query for fifth question

--- Queries.sql
+++ Queries.sql
@@ -24,3 +24,12 @@ FROM employee e
 JOIN city c ON e.cityid = c.cityid
 WHERE c.cityid = 2 and e.jobid = 1;
 
+--Ispis broja narudžbi za svaki restoran u Splitu tijekom 2023. godine.
+SELECT count(o.orderid) as num_orders,
+	r.name as restaurant_name
+FROM ORDERS O
+JOIN RESTAURANT R ON O.RESTAURANTID = R.RESTAURANTID
+JOIN CITY C ON R.CITYID = C.CITYID
+WHERE R.CITYID = 1
+GROUP BY R.RESTAURANTID
+
