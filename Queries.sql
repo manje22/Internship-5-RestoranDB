1bf057e1538c9c0aea7247029a7738ab7c1d086a
manje22 <uvodicmariela@gmail.com>
2024-12-06 10:17:39 +0100

    added query for fourth question

--- Queries.sql
+++ Queries.sql
@@ -6,3 +6,21 @@ SELECT * FROM ORDERS WHERE(
 	EXTRACT(YEAR FROM dateoforder) = 2023 AND
 	total > 50.00
 )
+
+--Ispis svih dostavljača s više od 100 uspješno izvršenih dostava do danas.
+
+SELECT e.employeeid,
+	e.name,
+	COUNT(D.ORDERID)
+FROM employee e
+JOIN DELIVERIES D ON E.EMPLOYEEID = D.EMPLOYEEID
+GROUP BY E.EMPLOYEEID, E.NAME
+
+--Ispis svih kuhara koji rade u restoranima u Zagrebu.
+SELECT e.name,
+       c.name,
+	   e.jobid
+FROM employee e
+JOIN city c ON e.cityid = c.cityid
+WHERE c.cityid = 2 and e.jobid = 1;
+
